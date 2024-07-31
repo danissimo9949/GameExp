7a9973f1cd25ec54c4f7d77aaeec7ccdb79e0aad
Zayats <danissimo228337@gmail.com>
2024-07-31 15:51:51 +0300

    Project model before migration applying

--- GameExp/main_app/models.py
+++ GameExp/main_app/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 from django.utils.text import slugify
-from .utils import screenshot_upload_path
+from .utils import screenshot_upload_path, cover_image_upload_path, uploads_upload_path
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50, blank=False, null=False)
+
+    class Meta:
+        verbose_name = 'Genre'
+        verbose_name_plural = 'Genres'
+
 
 class Project(models.Model):
 
@@ -28,12 +36,26 @@ class Project(models.Model):
     """
     title = models.CharField(max_length=255, blank=False)
     project_url = models.URLField(max_length=255)
-    short_description = models.CharField(max_length=255)
-    cover_img = models.ImageField(upload_to=f'projects/{slugify(title)}/cover-image/')
-    gameplay_video = models.URLField(max_length=255)
-
-    suggested_donate = models.DecimalField(verbose_name='donate', max_digits=6, decimal_places=2, default=2.00)
-    price = models.DecimalField(verbose_name='price', max_digits=6, decimal_places=2, default=1.00)
+    short_description = models.CharField(max_length=255, blank=True, null=True)
+    cover_img = models.ImageField(upload_to=cover_image_upload_path, blank=True, null=True)
+    gameplay_video = models.URLField(max_length=255, blank=True, null=True)
+    details = models.TextField(blank=True, null=True)
+    genre = models.ForeignKey(Genre, on_delete=models.PROTECT)
+    
+    suggested_donate = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        default=2.00,
+        blank=True,
+        null=True
+    )
+    price = models.DecimalField(
+        max_digits=6, 
+        decimal_places=2, 
+        default=1.00,
+        blank=True,
+        null=True
+    )
     
     """
     Choise fields
@@ -44,10 +66,21 @@ class Project(models.Model):
     """
     Boolean field for state
     """
-    is_free_or_donate = models.BooleanField(verbose_name='Free or donate project', default=True)
-    is_paid = models.BooleanField(default=False, blank=True)
-    is_free = models.BooleanField(default=False, blank=True)
+    is_free = models.BooleanField(default=False, blank=True, null=True)
+    is_community_disabled = models.BooleanField(verbose_name='community', default=False, blank=True, null=True)
+    have_commentary = models.BooleanField(verbose_name='commentary', default=True, blank=True, null=True)
+    have_discussion_board = models.BooleanField(verbose_name='discussion board', default=False, blank=True, null=True)
+    
+    """
+    Project restriction
+    """
+    is_draft = models.BooleanField(verbose_name='draft', default=False, blank=True, null=True)
+    is_restricted = models.BooleanField(verbose_name='restricted', default=False, blank=True, null=True)
+    is_public = models.BooleanField(verbose_name='public restriction', default=False, blank=True, null=True)
 
+    class Meta:
+        verbose_name = 'Project'
+        verbose_name_plural = 'Projects'
 
 
 
@@ -55,3 +88,15 @@ class ProjectScreenshot(models.Model):
     project = models.ForeignKey(Project, related_name='project_screens', on_delete=models.CASCADE)
     screenshot = models.ImageField(upload_to=screenshot_upload_path)
 
+    class Meta:
+        verbose_name = 'Screenshot'
+        verbose_name_plural = 'Screenshots'
+
+
+class ProjectUploadedFile(models.Model):
+    project = models.ForeignKey(Project, related_name='project_uploads', on_delete=models.CASCADE)
+    file = models.FileField(upload_to=uploads_upload_path)
+
+    class Meta:
+        verbose_name = 'Upload file'
+        verbose_name_plural = 'Upload files'
--- GameExp/main_app/utils.py
+++ GameExp/main_app/utils.py
@@ -3,3 +3,11 @@ from django.utils.text import slugify
 def screenshot_upload_path(instance, filename):
     project_slug = slugify(instance.project.title)
     return f'projects/{project_slug}/screenshots/{filename}'
+
+def cover_image_upload_path(instance, filename):
+    project_slug = slugify(instance.project.title)
+    return f'projects/{project_slug}/cover-images/{filename}'
+
+def uploads_upload_path(instance, filename):
+    project_slug = slugify(instance.project.title)
+    return f'projects/{project_slug}/uploads/{filename}'
